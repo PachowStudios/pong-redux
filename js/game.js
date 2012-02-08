5cc0ed309cd8be19115383e3f725fcc6e869bb57
Ryan Shea <ryansworld10@gmail.com>
2012-02-08 10:28:04 -0800

    Release v1.1 - Added better collision detection - Ball can now be directed up or down by hitting the top or bottom 1/4 of the paddle - Added paddle expansion powerup (there is no way to get this yet)

--- js/game.js
+++ js/game.js
@@ -8,11 +8,6 @@ canvas = document.getElementById("canvas");
 ctx = canvas.getContext("2d");
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
-//setup overlay
-overlay = document.getElementById("overlay");
-ctxOverlay = overlay.getContext("2d");
-ctxOverlay.canvas.width = window.innerWidth;
-ctxOverlay.canvas.height = window.innerHeight;
 //var arrays
 KEY = {
 	UP: 38,
@@ -21,13 +16,15 @@ KEY = {
 	S: 83,
 	D: 68,
 	P: 80,
-	O: 79
+	O: 79,
+	I: 73
 }
 pingpong = {
 	fps: 60,
 	pressedKeys: [],
+	debug: false,
 	paused: false,
-	ball: {
+	ball: {         
 		x: null,
 		y: null,
 		width: 20,
@@ -48,6 +45,7 @@ pingpong = {
 		speedModifier: 0,
 		color: "#FFFFFF",
 		expanded: false,
+		shrunk: false,
 	},
 	paddleB: {
 		x: null,
@@ -59,6 +57,7 @@ pingpong = {
 		speedModifier: 0,
 		color: "#FFFFFF",
 		expanded: false,
+		shrunk: false,
 	},
 	scoreA: {
 		value: 0,
@@ -94,7 +93,7 @@ pingpong = {
 		version: true
 	}
 };
-//clarify some vars
+//redefine sub-arrays
 ball = pingpong.ball;
 paddleA = pingpong.paddleA;
 paddleB = pingpong.paddleB;
@@ -143,11 +142,11 @@ pingpong.gameloop = setInterval(gameloop,1000/pingpong.fps);
 /************/
 
 function gameloop() {
-renderGraphics();
 checkInput();
 moveBall();
 checkCollision();
 checkComplete();
+renderGraphics();
 }
 
 function renderGraphics() {
@@ -155,8 +154,6 @@ function renderGraphics() {
 if (ctx.canvas.width != window.innerWidth || ctx.canvas.height != window.innerHeight) {
 	ctx.canvas.width = window.innerWidth;
 	ctx.canvas.height = window.innerHeight;
-	ctxOverlay.canvas.width = ctx.canvas.width;
-	ctxOverlay.canvas.height = ctx.canvas.height;
 	paddleA.y = (ctx.canvas.height - paddleA.height) / 2;
 	paddleB.y = (ctx.canvas.height - paddleB.height) / 2;
 	paddleB.x = ctx.canvas.width - 100 - paddleB.width;
@@ -186,69 +183,87 @@ if (render.scoreB) {
 	ctx.fillStyle = scoreB.color;
 	ctx.fillText(scoreB.value,scoreB.x,scoreB.y);
 }
+//draw win screen
 if (render.complete) {
 	ctx.fillStyle = complete.color;
 	ctx.fillText(complete.text,complete.x,complete.y);
 }
+//draw pause screen
 if (render.paused) {
 	ctx.fillStyle = "#FFFFFF";
 	ctx.fillText("Paused",ctx.canvas.width / 2,20);
 }
 ctx.font = "60px pong";
+//draw notafication
 if (render.notafication) {
 	ctx.fillStyle = notafication.color;
 	ctx.fillText(notafication.text,notafication.x,notafication.y);
 }
+//draw version number
 if (render.version) {
 	ctx.font = "20px pong";
 	ctx.textAlign = "right";
-	ctx.fillText("v1.0",ctx.canvas.width - 5,5);
+	ctx.fillText("v1.1",ctx.canvas.width - 5,5);
 }
 }
 
 function checkInput() {
+//move paddleB up
 if (pingpong.pressedKeys[KEY.UP].isDown) {
 	if (paddleB.y > 0) {
 		paddleB.y -= paddleB.speed + paddleB.speedModifier;
 	}
 }
+//mobe paddleb down
 if (pingpong.pressedKeys[KEY.DOWN].isDown) {
 	if (paddleB.y + paddleB.height + paddleB.heightModifier< ctx.canvas.height) {
 		paddleB.y += paddleB.speed + paddleB.speedModifier;
 	}
 }
+//move paddleA up
 if (pingpong.pressedKeys[KEY.W].isDown) {
 	if (paddleA.y > 0) {
 		paddleA.y -= paddleA.speed + paddleA.speedModifier;
 	}
 }
+//move paddleA down
 if (pingpong.pressedKeys[KEY.S].isDown) {
 	if (paddleA.y + paddleA.height + paddleA.heightModifier < ctx.canvas.height) {
 		paddleA.y += paddleA.speed + paddleA.speedModifier;
 	}
 }
+//pause game
 if (pingpong.pressedKeys[KEY.P].isDown) {
 	if (!pingpong.pressedKeys[KEY.P].wasDown) {
 		pause();
 	}
 }
-/*if (pingpong.pressedKeys[KEY.D].isDown) {
+//slow down ball and paddles [DEBUG]
+if (pingpong.pressedKeys[KEY.D].isDown && pingpong.debug) {
 	if (!pingpong.pressedKeys[KEY.D].wasDown) {
 		ball.speedModifier -= 4.5;
 		paddleA.speedModifier -= 9;
 		paddleB.speedModifier -= 9;
 	}
 } 
-else if (pingpong.pressedKeys[KEY.D].wasDown) {
+//return ball and paddles to normal speed [DEBUG]
+else if (pingpong.pressedKeys[KEY.D].wasDown && pingpong.debug) {
 	ball.speedModifier += 4.5;
 	paddleA.speedModifier += 9;
 	paddleB.speedModifier += 9;
 }
-if (pingpong.pressedKeys[KEY.O].isDown) {
+//test paddle expansion [DEBUG]
+if (pingpong.pressedKeys[KEY.O].isDown && pingpong.debug) {
 	if (!pingpong.pressedKeys[KEY.O].wasDown) {
-		expand(paddleA,2,40,10);
+		expand(paddleB,2,40,3);
 	}
-}*/
+}
+//test paddle shrinkage [DEBUG]
+if (pingpong.pressedKeys[KEY.I].isDown && pingpong.debug) {
+	if (!pingpong.pressedKeys[KEY.I].wasDown) {
+		shrink(paddleB,2,-40,3);
+	}
+}
 for (var keyCode in KEY) {
 	if (KEY.hasOwnProperty(keyCode)) {
 		pingpong.pressedKeys[KEY[keyCode]].wasDown = pingpong.pressedKeys[KEY[keyCode]].isDown;
@@ -284,30 +299,45 @@ ball.y += (ball.speed + ball.speedModifier) * ball.directionY;
 
 function checkCollision() {
 //left paddle
-if (ball.x < paddleA.x + paddleA.width && ball.x > paddleA.x) {
+if (ball.x <= paddleA.x + paddleA.width && ball.x + ball.width >= paddleA.x) {
 	if (ball.y <= paddleA.y + paddleA.height + paddleA.heightModifier && ball.y + ball.height >= paddleA.y) {
 		ball.directionX = 1;
+		if (ball.y + (ball.height / 2) <= paddleA.y + (paddleA.height * 0.25)){
+			ball.directionY = -1;
+		}
+		else if (ball.y + (ball.height / 2) >= paddleA.y + (paddleA.height * 0.75)) {
+			ball.directionY = 1;
+		}
 		increaseBallSpeed();
 	}
 }
 //right paddle
-if (ball.x + ball.width >= paddleB.x && ball.x + ball.width <= paddleB.x + paddleB.width) {
+if (ball.x + ball.width >= paddleB.x && ball.x <= paddleB.x + paddleB.width) {
 	if (ball.y <= paddleB.y + paddleB.height + paddleB.heightModifier && ball.y + ball.height >= paddleB.y) {
 		ball.directionX = -1;
+		if (ball.y + (ball.height / 2) <= paddleB.y + (paddleB.height * 0.25)){
+			ball.directionY = -1;
+		}
+		else if (ball.y + (ball.height / 2) >= paddleB.y + (paddleB.height * 0.75)) {
+			ball.directionY = 1;
+		}
 		increaseBallSpeed();
 	}
 }
 }
 
 function checkComplete() {
+//check scoreA
 if (scoreA.value >= 10) {
 	complete.text = "Player 1 wins!";
 	complete.value = true;
 }
+//check scoreB
 if (scoreB.value >= 10) {
 	complete.text = "Player 2 wins!";
 	complete.value = true;
 }
+//if complete, end game
 if (complete.value == true) {
 	render.scoreA = false;
 	render.scoreB = false;
@@ -322,13 +352,16 @@ if (complete.value == true) {
 /*********************/
 
 function probability(max) {
+//generate a random whole number between 0 and max
 return Math.floor(Math.random()*max);
 }
-
+ 
 function respawnBall() {
+//reset ball coords
 ball.x = (ctx.canvas.width / 2) - (ball.width / 2);
 ball.y = probability(ctx.canvas.height / 2) + (ctx.canvas.height / 4);
 ball.speedModifier = 0;
+//randomize ball direction
 if (probability(2) == 0) {
 	ball.directionX = 1;
 }
@@ -344,6 +377,7 @@ else {
 }
 
 function increaseBallSpeed() {
+//get the probability of 1 in 3 and increase ball speed
 if (probability(3) == 1) {
 	ball.speedModifier++;
 	notafication("Speed Increased!",ctx.canvas.width / 2,ctx.canvas.height - 100,"#FFFFFF",3,750);
@@ -351,11 +385,14 @@ if (probability(3) == 1) {
 }
 
 function notafication(text,x,y,color,count,interval) {
+//apply passed vars to global vars
 notafication.text = text;
 notafication.x = x;
 notafication.y = y;
 notafication.color = color;
+//enable notafication rendering
 render.notafication = true;
+//toggle notafication x times
 clearInterval(pingpong.flashTimer);
 pingpong.flashTimer = setInterval(function() {
 	count--;
@@ -373,13 +410,17 @@ pingpong.flashTimer = setInterval(function() {
 }
 
 function pause() {
+//if not paused, pause game
 if (!pingpong.paused) {
+	//stop game loop and change render toggles
 	clearInterval(pingpong.gameloop);
 	render.scoreA = false;
 	render.scoreB = false;
 	render.paused = true;
+	//re-enable rendering seperate from gameloop
 	pingpong.pauseloop = setInterval(function() {
 		renderGraphics();
+		//key listener for unpause
 		if (pingpong.pressedKeys[KEY.P].isDown) {
 			if (!pingpong.pressedKeys[KEY.P].wasDown) {
 				pause();
@@ -394,6 +435,7 @@ if (!pingpong.paused) {
 	pingpong.paused = true;
 }
 else {
+	//disable pauseloop, re-enable gameloop
 	clearInterval(pingpong.pauseloop);
 	render.paused = false;
 	render.scoreA = true;
@@ -408,27 +450,67 @@ else {
 /************/
 
 function expand(paddle,inc,amt,time) {
-if (paddle.expanded == false) {
+//animate expansion if not active
+if (!paddle.expanded) {
+	//get current height modifier for reference
+	var origM = paddle.heightModifier;
 	paddle.expanded = true;
-	pingpong.expandloop = setInterval(function() {
+	paddle.expand_expandloop = setInterval(function() {
 		paddle.heightModifier += inc;
 		paddle.y -= inc / 2;
-		if (paddle.heightModifier >= amt) {
-			clearInterval(pingpong.expandloop);
+		if (paddle.heightModifier >= amt + origM) {
+			clearInterval(paddle.expand_expandloop);
 		}
 	},1000/pingpong.fps);
+	//return to normal size after set time
 	setTimeout(function() {
-		pingpong.shrinkloop = setInterval(function() {
+		//update current height modifier in case it may have changed due to multiple powerups
+		var newM = paddle.heightModifier;
+		paddle.expand_shrinkloop = setInterval(function() {
 			paddle.heightModifier -= inc;
 			paddle.y += inc / 2;
-			if (paddle.heightModifier <= 0) {
-				clearInterval(pingpong.shrinkloop);
+			if (paddle.heightModifier <= newM - amt) {
+				clearInterval(paddle.expand_shrinkloop);
 				paddle.expanded = false;
 			}
 		},1000/pingpong.fps);
 	},1000*time);
 }
-else if (paddle.expanded == true) {
-	return;
+//end if powerup is already active
+else if (paddle.expanded) {
+	return
+}
+}
+
+function shrink(paddle,inc,amt,time) {
+//animate shrinkage if not active
+if (!paddle.shrunk) {
+	//get current height modifier for reference
+	var origM = paddle.heightModifier;
+	paddle.shrunk = true;
+	paddle.shrink_shrinkloop = setInterval(function() {
+		paddle.heightModifier -= inc;
+		paddle.y += inc / 2;
+		if (paddle.heightModifier <= amt + origM) {
+			clearInterval(paddle.shrink_shrinkloop);
+		}
+	},1000/pingpong.fps);
+	//return to normal size after set time
+	setTimeout(function() {
+		//update current height modifier in case it may have changed due to multiple powerups
+		var newM = paddle.heightModifier;
+		paddle.shrink_expandloop = setInterval(function() {
+			paddle.heightModifier += inc;
+			paddle.y -= inc / 2;
+			if (paddle.heightModifier >= newM - amt) {
+				clearInterval(paddle.shrink_expandloop);
+				paddle.shrunk = false;
+			}
+		},1000/pingpong.fps);
+	},1000*time);
+}
+//end if powerup is already active
+else if (paddle.shrunk) {
+	return
 }
 }
